4bd6c8c259c0dc325198d303642e92e4aca82808
dcamelog <69636867+dcamelog@users.noreply.github.com>
2022-04-02 07:17:55 -0500

    actualizacion Front

--- Arquisoft/arquitectura_estrella/arquitectura_estrella/urls.py
+++ Arquisoft/arquitectura_estrella/arquitectura_estrella/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings 
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('estado_academico/',include('estado_academico.urls')),
     path('registro_notas/',include('registro_notas.urls')),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Arquisoft/arquitectura_estrella/registro_notas/views.py
+++ Arquisoft/arquitectura_estrella/registro_notas/views.py
@@ -1,6 +1,18 @@
+import re
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
     return render(request, 'interfaz_carga.html')
+
+def simple_upload(request):
+    if request.method == 'POST' and request.FILES['myfile']:
+        myfile=request.FiLES['myfile']
+        fs = FileSystemStorage()
+        filename= fs.save(myfile.name, myfile)
+        uploades_file_url =fs.url(filename)
+        return render(request, 'app/index.html', {
+            'uploaded_file_url':uploaded_file_url
+        })
+    return render(request,'interfaz_carga.html')
