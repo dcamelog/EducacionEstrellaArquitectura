f6e74dce21c1d1362a447a69f1c93c40aed40849
dcamelog <69636867+dcamelog@users.noreply.github.com>
2022-04-02 05:37:30 -0500

    Actualizacioinn de front-end
    front actualizado

--- Arquisoft/arquitectura_estrella/arquitectura_estrella/urls.py
+++ Arquisoft/arquitectura_estrella/arquitectura_estrella/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
 urlpatterns = [
+    path('estado_academico/',include('estado_academico.urls')),
+    path('registro_notas/',include('registro_notas.urls')),
     path('admin/', admin.site.urls),
 ]
