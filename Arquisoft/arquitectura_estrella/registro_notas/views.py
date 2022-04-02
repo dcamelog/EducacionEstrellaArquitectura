324ee1d58624e7319fabeb2b6d852b1d437ac3c1
dcamelog <69636867+dcamelog@users.noreply.github.com>
2022-04-02 11:09:04 -0500

    mitad del front perfectamente funcional

--- Arquisoft/arquitectura_estrella/registro_notas/views.py
+++ Arquisoft/arquitectura_estrella/registro_notas/views.py
@@ -2,17 +2,19 @@ import re
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
+from django.conf import settings
 
 def index(request):
     return render(request, 'interfaz_carga.html')
 
 def simple_upload(request):
     if request.method == 'POST' and request.FILES['myfile']:
-        myfile=request.FiLES['myfile']
+        myfile=request.FILES['myfile']
         fs = FileSystemStorage()
         filename= fs.save(myfile.name, myfile)
-        uploades_file_url =fs.url(filename)
-        return render(request, 'app/index.html', {
+        uploaded_file_url =fs.url(filename)
+        return render(request, 'interfaz_carga.html', {
             'uploaded_file_url':uploaded_file_url
         })
     return render(request,'interfaz_carga.html')
