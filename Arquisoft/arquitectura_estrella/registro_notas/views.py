41f3123507b568c88b845f002d6b43b30653c60c
Jesús Alejandro Dávila <l1nkg4mer555@gmail.com>
2022-05-13 22:04:57 -0500

    redirect

--- Arquisoft/arquitectura_estrella/registro_notas/views.py
+++ Arquisoft/arquitectura_estrella/registro_notas/views.py
@@ -1,5 +1,5 @@
 import re
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.core.files.storage import FileSystemStorage
