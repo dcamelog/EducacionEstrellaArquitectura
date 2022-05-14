99d258eaa977cdf7a919ef6e8c033799b22c26a9
Jesús Alejandro Dávila <l1nkg4mer555@gmail.com>
2022-05-13 22:02:23 -0500

    redirect

--- Arquisoft/arquitectura_estrella/registro_notas/views.py
+++ Arquisoft/arquitectura_estrella/registro_notas/views.py
@@ -33,7 +33,10 @@ def logout(request):
     return_to = 'http://52.201.214.34:8000/registro_notas' 
     return redirect(f'https://{domain}/v2/logout?client_id={client_id}&returnTo={return_to}')
 
-def hand_crafted_redirect_view(request):
+def hand_crafted_redirect(request):
   response = HttpResponse(status=500)
   response['Location'] = '/redirect/fail/'
   return response
+
+def redirect(request):
+    return redirect('/redirect/fail/')
